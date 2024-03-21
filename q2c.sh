995fedcdbfc6f0273bd519d43c8680c26fbd5b88
guy <guy.duenias@post.runi.ac.il>
2024-03-22 00:07:09 +0200

    interesting query fix

--- q2c.sh
+++ q2c.sh
@@ -2,7 +2,7 @@
 
 # checks what are the top 5 most wanted jobs in NYC
 interesting_query='SELECT "Job Category", COUNT(*) AS count_by_category FROM job_postings GROUP BY "Job Category" ORDER BY count_by_category DESC LIMIT 5;'
-curl -X POST http://127.0.0.1:5000/ -d "$interesting _query"
+curl 127.0.0.1:5000 -X POST -d "$interesting_query"
 
 
 
@@ -12,3 +12,4 @@ curl -X POST http://127.0.0.1:5000/ -d "$interesting _query"
 #Finance, Accounting, & Procurement|294
 #Building Operations & Maintenance|269
 #Legal Affairs|263
+# "Engineering, Architecture, & Planning" topping NYC job postings highlights investment in infrastructure and sustainability, showcasing the city's evolving, diverse economic priorities.
